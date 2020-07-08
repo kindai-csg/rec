f751c25d6cedbbec57b37e299c1265b1868daaa9
K.Tsukimoto <t.29.wuq@gmail.com>
2020-07-09 04:40:32 +0900

    :+1: 本番用にコメントアウトの解除パスの修正

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3.6
-# import cv2
+import cv2
 import base64
-# import threading
+import threading
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO, emit
 
-# from record import recording_save_img
+from record import recording_save_img
 
-# capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on('live_1', namespace = '/')
 def live_1():
-    with open('/src/sample.jpg', 'rb') as f:
+    with open('/home/recorder/rec.kindai-csg.dev/latest.jpg', 'rb') as f:
         emit('live_1', { 'data': base64.b64encode(f.read()).decode('ascii') })
 
 @socketio.on('live_2', namespace = '/')
 def live_2():
-    with open('/src/sample.jpg', 'rb') as f:
+    with open('/home/recorder/rec.kindai-csg.dev/latest172.24.18.2.jpg', 'rb') as f:
         emit('live_2', { 'data': base64.b64encode(f.read()).decode('ascii') })
 
 def main():
-    # record_thread = threading.Thread(target=recording_save_img, args=(capture,))
-    # record_thread.start()
+    record_thread = threading.Thread(target=recording_save_img, args=(capture,))
+    record_thread.start()
     socketio.run(app, port=8080, debug=True, host='0.0.0.0')
 
 
